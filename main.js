8851205dc7b267f21d00809168d334cf843e8841
ArisaKamishima <arisa.6.snsd@icloud.com>
2020-06-03 22:53:26 +0900

    changed scss

---
+++ main.js
@@ -0,0 +1,58 @@
+// const mySwiper = new Swiper(".swiper-container", {
+//   // Optional parameters
+//   //   direction: "vertical",
+//   loop: true,
+//   effct: "coverflow",
+
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination"
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev"
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar"
+//   }
+// });
+
+var pics_src = new Array(
+  "img_mainpage.png",
+  "img2_mainpage.png",
+  "img3_mainpage.png"
+);
+var num = -1;
+
+slideshow_timer();
+
+function slideshow_timer() {
+  if (num == 2) {
+    num = 0;
+  } else {
+    num++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+  setTimeout("slideshow_timer()", 4000);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  var plusButton = document.querySelector(".btn-wrapper");
+  var imgInfo = document.querySelector(".img-info");
+  var closeButton = document.querySelector(".img-info__close-btn");
+  // after clicking, turn on img-info
+  plusButton.addEventListener("click", function() {
+    // add class name "img-info--active" to imgInfo
+    imgInfo.classList.add("img-info--active");
+  });
+
+  // after clicking, turn off img-info
+  closeButton.addEventListener("click", function() {
+    // remove class name "img-info--active" to imgInfo
+    imgInfo.classList.remove("img-info--active");
+  });
+});
